7fc86a5025e30c73642028259674a5913bf27f0e
nkyancen <nkyancen@yandex.ru>
2025-06-07 22:56:02 +0300

    some changes

--- app/src/main/java/ru/nkyancen/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/nkyancen/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package ru.nkyancen.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +11,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+
+        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+                startActivity(searchIntent)
+            }
+        }
+
+        buttonSearch.setOnClickListener(buttonSearchClickListener)
+
+        buttonMedia.setOnClickListener {
+            val mediaLibraryIntent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
+            startActivity(mediaLibraryIntent)
+        }
+
+        buttonSettings.setOnClickListener {
+            val settingsIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(settingsIntent)
+        }
 
     }
 }
