f43dbfa463d2f3d568a4fcc9466e8a8f58e0f60a
nkyancen <nkyancen@yandex.ru>
2025-06-25 23:19:00 +0300

    Переключение тем день/ночь

--- app/src/main/java/ru/nkyancen/playlistmaker/MainActivity.kt
+++ app/src/main/java/ru/nkyancen/playlistmaker/MainActivity.kt
@@ -2,7 +2,6 @@ package ru.nkyancen.playlistmaker
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
 
@@ -16,14 +15,10 @@ class MainActivity : AppCompatActivity() {
         val buttonMedia = findViewById<MaterialButton>(R.id.button_media)
         val buttonSettings = findViewById<MaterialButton>(R.id.button_settings)
 
-        val buttonSearchClickListener: View.OnClickListener = object : View.OnClickListener {
-            override fun onClick(v: View?) {
-                val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
-                startActivity(searchIntent)
-            }
+        buttonSearch.setOnClickListener {
+            val searchIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(searchIntent)
         }
-
-        buttonSearch.setOnClickListener(buttonSearchClickListener)
 
         buttonMedia.setOnClickListener {
             val mediaLibraryIntent = Intent(this@MainActivity, MediaLibraryActivity::class.java)
--- app/src/main/java/ru/nkyancen/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ru/nkyancen/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,11 @@
 package ru.nkyancen.playlistmaker
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
@@ -15,5 +19,42 @@ class SettingsActivity : AppCompatActivity() {
         buttonBack.setNavigationOnClickListener {
             finish()
         }
+
+        val switcher = findViewById<SwitchCompat>(R.id.switch_dark_theme)
+
+        switcher.isChecked = isNightThemeOn(this@SettingsActivity)
+
+        switcher.setOnCheckedChangeListener { _, isChecked ->
+            val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+            if (currentNightMode == AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+            } else if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    private fun isNightThemeOn(context: Context): Boolean {
+        val currentNightMode = AppCompatDelegate.getDefaultNightMode()
+
+        return when (currentNightMode) {
+            AppCompatDelegate.MODE_NIGHT_YES -> {
+                true
+            }
+
+            AppCompatDelegate.MODE_NIGHT_NO -> {
+                false
+            }
+
+            else -> {
+                val configuration = context.resources.configuration
+                val uiMode = configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
+
+                uiMode == Configuration.UI_MODE_NIGHT_YES
+            }
+        }
+
     }
 }
